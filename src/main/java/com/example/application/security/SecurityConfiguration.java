35c8b803cbc8fd937a0b66cdf9d114168cd2ec7f
start.vaadin.com <no-reply@vaadin.com>
2026-04-27 07:32:39 +0000

    Generated project

---
+++ src/main/java/com/example/application/security/SecurityConfiguration.java
@@ -0,0 +1,38 @@
+package com.example.application.security;
+
+import static com.vaadin.flow.spring.security.VaadinSecurityConfigurer.vaadin;
+
+import com.example.application.views.login.LoginView;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfiguration {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain vaadinSecurityFilterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/images/*.png", "/*.css").permitAll());
+
+        // Icons from the line-awesome addon
+        http.authorizeHttpRequests(authorize -> authorize.requestMatchers("/line-awesome/**").permitAll());
+
+        http.with(vaadin(), vaadin -> {
+            vaadin.loginView(LoginView.class);
+        });
+
+        return http.build();
+    }
+
+}
